56e8d975fbf0d7da1538954d2cd84e6efe7cbb73
sm-nazmus-sakib <nazmussakibnwu@gmal.com>
2022-10-12 12:04:31 +0600

    some change in the design

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -1,7 +1,10 @@
 import React from "react";
 
 const Blog = () => {
-  return <div className="md:px-4 px-2 md:mt-5 mt-5">This is Blog</div>;
+  return <div className="md:px-4 px-2 md:mt-5 mt-5 bg-slate-400">
+    <h1 className="text-2xl py-2">What does the purpose of react router?</h1>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis commodi placeat assumenda. Ipsa dolores beatae nam, dicta fugit aliquid eos nesciunt, repellat iusto reprehenderit distinctio delectus ut animi ratione aut.</p>
+  </div>;
 };
 
 export default Blog;
